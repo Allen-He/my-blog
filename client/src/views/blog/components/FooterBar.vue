233b234a6b8d2aaea2c771e1eded7ef219327bb1
Allen-He <2426567439@qq.com>
2021-10-15 09:57:07 +0800

    feat(客户端): 开发FooterBar组件，即底部导航条

--- client/src/views/blog/components/FooterBar.vue
+++ client/src/views/blog/components/FooterBar.vue
@@ -1,19 +1,36 @@
 <template>
   <footer class="footerBar">
-    <InfoItem/>
-    <InfoItem/>
-    <InfoItem/>
-    <InfoItem/>
-    <InfoItem/>
+    <div class="item">
+      <i class="iconfont">&#xe608;</i>
+      <a href="https://vuepress-theme-reco.recoluan.com/" target="_blank">vuepress-theme-reco@1.6.6</a>
+    </div>
+    <div class="item">
+      <i class="iconfont">&#xe7b7;</i>
+      <a href="/" target="_blank">而已的博客</a>
+    </div>
+    <div class="item">
+      <i class="iconfont">&#xe6b5;</i>
+      <span>而已&nbsp;&nbsp;&nbsp;{{ rangeYear }}</span>
+    </div>
+    <div class="item">
+      <i class="iconfont">&#xe648;</i>
+      <span>99999</span>
+    </div>
   </footer>
 </template>
 
 <script>
-import InfoItem from './InfoItem.vue';
-
 export default {
-  components: {
-    InfoItem,
+  data() {
+    return {
+      birthYear: 2021,
+      nowYear: new Date().getFullYear(),
+    };
+  },
+  computed: {
+    rangeYear() {
+      return `${this.birthYear}-${this.nowYear}`;
+    },
   },
 };
 </script>
@@ -27,5 +44,28 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  .item {
+    height: 20px;
+    margin-left: 24px;
+    display: flex;
+    align-items: center;
+
+    &:first-child {
+      margin: 0;
+    }
+
+    .iconfont {
+      font-size: 16px;
+      color: #7F7F7F;
+      margin-right: 8px;
+    }
+    a,
+    span {
+      font-size: 14px;
+      font-weight: 500;
+      color: #3eaf7c;
+      vertical-align: middle;
+    }
+  }
 }
 </style>
