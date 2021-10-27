a6cd694bdfe0b752e94c36bea6ddc5230eb7303d
Allen-He <2426567439@qq.com>
2021-10-27 16:10:38 +0800

    feat(服务器): 过滤登录接口返回的敏感信息

--- server/routes/api/admin.js
+++ server/routes/api/admin.js
@@ -14,8 +14,9 @@ router.post('/login', asyncHandler(async (req, res, next) => {
   if(data) { //登陆成功，发布token（包含当前admin对应的id）
     const idVal = data.id;
     jwt.publish(res, undefined, { id: idVal });
-    // 过滤敏感数据（密码）再返回
+    // 过滤敏感数据（密码、id）再返回
     delete data.loginPwd;
+    delete data.id;
   }
   return data;
 }));
@@ -23,8 +24,9 @@ router.post('/login', asyncHandler(async (req, res, next) => {
 router.get('/whoami', asyncHandler(async (req, res, next) => {
   const data = await adminServ.getAdminById(req.userId); //req.userId是由tokenMiddleware添加的属性，即：当前登录用户对应的id
   if(data) {
-    // 过滤敏感数据（密码）再返回
+    // 过滤敏感数据（密码、id）再返回
     delete data.loginPwd;
+    delete data.id;
   }
   return data;
 }));
