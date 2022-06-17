3cbc400a85f0e4e700121cacd71ad557b8694cde
Allen_He <hcx.fe@qq.com>
2022-06-17 11:55:11 +0800

    fix(): 删除多余的注释

--- server/index.js
+++ server/index.js
@@ -1,11 +1,3 @@
 require('./models/init');
 require('./mock/init');
 require('./routes/index');
-
-// const adminServ = require('./services/adminServ');
-// adminServ.addAdmin({
-//   loginId: '18308365449',
-//   loginPwd: 'hcx147852369',
-//   nickname: '继续努力就好'
-// });
-// adminServ.login('18308365449', 'hcx147852369').then(res => console.log(res));
